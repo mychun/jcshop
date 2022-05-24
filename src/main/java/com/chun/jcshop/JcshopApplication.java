d987af1e0dfa68de7fb1fc890f006a37976b64b3
chunxhwan <chunxhwan@126.com>
2022-05-24 11:00:09 +0800

    ## jcshop初始化项目提交

---
+++ src/main/java/com/chun/jcshop/JcshopApplication.java
@@ -0,0 +1,13 @@
+package com.chun.jcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chun/jcshop/JcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chun.jcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
